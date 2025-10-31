fcfc7b929859a56e01f9dfe550ebc864c1b95058
ffelipemotta <144288209+ffelipemotta@users.noreply.github.com>
2025-10-31 23:52:44 +0000

    adicionar conexão com banco de dados

--- index.js
+++ index.js
@@ -3,16 +3,16 @@ const app = express();
 
 app.use(express.json());
 
+// Rota principal
 app.get("/", (req, res) => {
-    res.json({ message: "API rodando dentro do Codespaces!"});
-
+  res.json({ message: "API rodando dentro do Render!" });
 });
+
+// Importa rotas de usuários
+const userRoutes = require("./routes/users");
+app.use("/users", userRoutes);
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
-    console.log('Servidor rodando na porta ${port}');
+  console.log(`Servidor rodando na porta ${port}`);
 });
-
-//Importa as rotas de usuarios
-const userRouter = require("./routes/users");
-app.use("/users", userRouter);
